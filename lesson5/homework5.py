0ab02328fb3ab0c4ea7b84b271ee0cba4e9320b8
Elvirience <92172008+Elvirience@users.noreply.github.com>
2021-10-29 13:11:14 +0300

    Update homework5.py

--- lesson5/homework5.py
+++ lesson5/homework5.py
@@ -71,6 +71,27 @@ def num6(result_dict={}):
         print(result_dict)
 
         
+def num7(all_profit=0, count=0, firm_dict={}):
+    with open('ex7.txt', encoding='UTF - 8') as fout:
+        for line in fout:
+            line = line.split()
+            profit = int(line[2]) - int(line[3])
+            if profit > 0:
+                all_profit += profit
+            else:
+                pass
+            count += 1
+            unit_dict = {line[0]: profit}
+            firm_dict.update(unit_dict)
+        my_list = [firm_dict, {'Average profit': all_profit / count}]
+        print(my_list)
+    """
+    json
+    """
+    with open('ex7.json','w' ,encoding='UTF - 8') as fin:
+        fin.write(json.dumps(my_list))
+     
+    
 """
 main():
 """
@@ -89,3 +110,5 @@ while q != '':
         num5()
     elif q == '6':
         num6()
+    elif q == '7':
+        num7()
